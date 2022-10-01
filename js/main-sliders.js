890cee7ac803f96f31ec48405bcf4cb1063e8f12
Vladislav <54555148+dbbgroup@users.noreply.github.com>
2022-10-01 20:27:48 +0300

    homepage carousels added

---
+++ js/main-sliders.js
@@ -0,0 +1,23 @@
+//products__item-track
+//product-big-slider
+//tecnology__slider-track
+
+$(function() {
+    // Owl Carousel
+    var owl1 = $(".owl-carousel");
+    owl1.owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true
+    });
+});
+
+$(function() {
+    // Owl Carousel
+    var owl1 = $(".owl-carousel2");
+    owl1.owlCarousel({
+        items: 2,
+        loop: true,
+        nav: true
+    });
+});
