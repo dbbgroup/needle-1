49b9f7fd751c5d66759644224ecb9f65ddf7c8f0
Vladislav <54555148+dbbgroup@users.noreply.github.com>
2022-10-10 09:38:20 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-import { toggleMenu } from "./menu.js";
-import { slider } from "./slider.js";
+const menu = document.querySelector(".menu");
+const menuCont = document.querySelector(".menu__content");
+const burger = document.querySelector(".burger");
+const closeBtn = document.querySelector(".menu__close-btn");
 
-window.addEventListener("DOMContentLoaded", () => {
-	toggleMenu();
-	slider(".products__item-track", ".products__item-dots");
+burger.addEventListener("click", () => {
+	menu.style.visibility = "visible";
+	menuCont.style.left = "0";
 });
+
+closeBtn.addEventListener("click", () => {
+	menu.style.visibility = "hidden";
+	menuCont.style.left = "-150%";
+});
+
+menu.addEventListener("click", (event) => {
+	if (!event.target.classList.contains("menu__content")) {
+		menu.style.visibility = "hidden";
+		menuCont.style.left = "-150%";
+	}
+});
+
+document.addEventListener("keydown", (event) => {
+	if (event.key === "Escape") {
+		menu.style.visibility = "hidden";
+		menuCont.style.left = "-150%";
+	}
+});
+
